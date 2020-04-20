82db3076524873caf23d19b80376116078452a91
Si Yun (Steven Kim) <dev@donga.ac.kr>
2020-04-20 17:45:31 +0900

    setClip 이용해서 특정 텍스쳐 표시
    setClip 이용해서 특정 텍스쳐표시
    Test.cpp - keyTest()

--- TowerDefense/game/Test.cpp
+++ TowerDefense/game/Test.cpp
@@ -6,6 +6,10 @@
 Texture* texBg;
 Texture* texTitle;
 
+Texture* texTable;
+iRect rtTable;
+iPoint offTable;
+
 void loadTest()
 {
 	texBg = createImage("assets/menu/menuBg.jpg");
@@ -14,6 +18,10 @@ void loadTest()
 	createPopStart();
 
 	showPopPress(true);
+
+	texTable = createImage("assets/menu/tableBg.jpg");
+	rtTable = iRectMake(devSize.width / 3, devSize.height / 3, devSize.width / 3, devSize.height / 3);
+	offTable = iPointMake(0, 0);
 }
 
 void freeTest()
@@ -22,6 +30,8 @@ void freeTest()
 	freeImage(texTitle);
 	freePopPress();
 	freePopStart();
+
+	freeImage(texTable);
 }
 
 void drawTest(float dt)
@@ -34,14 +44,17 @@ void drawTest(float dt)
 		0, 0, texBg->width, texBg->height, VCENTER | HCENTER,
 		r, r, 2, 0, REVERSE_NONE);
 
-	drawImage(texTitle, devSize.width/2, 50,
+	drawImage(texTitle, devSize.width / 2, 50,
 		0, 0, texTitle->width, texTitle->height, TOP | HCENTER,
 		0.6f, 0.6f, 2, 0, REVERSE_NONE);
 
 	drawPopPress(dt);
 	drawPopStart(dt);
 
-
+	setClip(rtTable.origin.x, rtTable.origin.y,
+		rtTable.size.width, rtTable.size.height);
+	drawImage(texTable, offTable.x, offTable.y, TOP | LEFT);
+	setClip(0, 0, 0, 0);
 
 #if 0
 	static float delta = 0.0f;
@@ -54,8 +67,49 @@ void drawTest(float dt)
 #endif
 }
 
+bool touching = false;
+iPoint prevPoint;
+
+void moveTable(iPoint& off, iPoint mp, const iRect& rt, const iSize& dstSize)
+{
+	off += mp;
+
+	float minX = rt.origin.x + rt.size.width - dstSize.width;
+	float maxX = rt.origin.x;
+	float minY = rt.origin.y + rt.size.height - dstSize.height;
+	float maxY = rt.origin.y;
+
+	if (off.y < minY) off.y = minY;
+	else if (off.y > maxY) off.y = maxY;
+	if (off.x < minX) off.x = minX;
+	else if (off.x > maxX) off.x = maxX;
+
+}
+
 void keyTest(iKeyState stat, iPoint point)
 {
+	if (stat == iKeyStateBegan)
+	{
+		prevPoint = point;
+		touching = true;
+	}
+	else if (stat == iKeyStateMoved)
+	{
+		if (touching)
+		{
+			moveTable(offTable, point - prevPoint, rtTable, iSizeMake(texTable->width, texTable->height));
+			
+
+				prevPoint = point;
+				
+		}
+	}
+	else if (stat == iKeyStateEnded)
+	{
+		touching = false;
+	}
+	return;
+
 	if (keyPopStart(stat, point))
 		return;
 
@@ -150,7 +204,7 @@ void createPopStart()
 
 	pop->openPosition = iPointMake(devSize.width / 2, devSize.height / 2);
 	pop->closePosition = iPointMake((devSize.width - 280) / 2,
-									(devSize.height - 350) / 2);
+		(devSize.height - 350) / 2);
 	popStart = pop;
 }
 
--- TowerDefense/lib/iSize.h
+++ TowerDefense/lib/iSize.h
@@ -3,6 +3,24 @@
 struct iSize
 {
 	float width, height;
+	
+	bool operator == (const iSize& s);
+	bool operator != (const iSize& s);
+
+	iSize operator + (const iSize& s);
+	iSize& operator += (const iSize& s);
+
+	iSize operator - (const iSize& s);
+	iSize& operator -=(const iSize& s);
+
+	iSize operator * (const float f);
+	iSize operator *= (const float f);
+
+	iSize& operator /= (const iSize& s);
+	iSize operator / (const float f);
+	iSize& operator /= (const float f);
+
 };
 
 iSize iSizeMake(float width, float height);
+// 넓이 구하는거 추가하기
